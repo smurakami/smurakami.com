34e218ca60ef1067bc6d6a4e2401089ca083ba57
s.murakami <smurakami.84@gmail.com>
2018-09-12 21:55:05 +0900

    描画を安定させ、タイトルを見やすく

--- src/App.js
+++ src/App.js
@@ -30,115 +30,91 @@ class Top extends Component {
   }
 }
 
-const Utils = {
-  sectionBorderHeight: () => window.innerWidth * Math.tan(Math.PI/12),
-  sectionBorderWidth: () => window.innerWidth,
-}
+const Title = () => {
+  const title_style = {
+    backgroundImage: 'url(/images/profile.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
 
-class Title extends Component {
-  constructor(props) {
-    super(props)
-    this.timer = null;
-
-    this.state = {
-      logo_height: 300,
-    }
+  const logo_style = {
+    height: window.innerHeight,
   }
 
-  componentDidMount() {
-    if (this.timer) { clearTimeout(this.timer) }
-    this.timer = setTimeout(() => {
-      this.setState({logo_height: this.refs.logo.clientHeight})
-    }, 100)
-  }
-
-  render() {
-    const {logo_height} = this.state;
-
-    const title_style = {
-      backgroundImage: 'url(/images/profile.jpg)',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: window.innerHeight + Utils.sectionBorderHeight(),
-    };
-
-    const dot_style = {
-      paddingBottom: Utils.sectionBorderHeight(),
-    }
-
-    return (
-      <div className="Title" style={title_style}>
-        <div className="dot-pattern" style={dot_style}>
-          <div className="container">
-            <div className="logo" ref='logo'>
-              <img 
-              className="hide-on-small-only" 
-              src="/images/logo_yoko.svg"
-              alt="ディープサラリーマン村上晋太郎" />
-              <img 
-              className="hide-on-med-and-up" 
-              src="/images/logo_tate.svg"
-              alt="ディープサラリーマン村上晋太郎" />
-            </div>
-          </div>
-        </div>
-      </div>
-      )
-  }
-}
-
-const TopTriangle = () => {
-  const style = {
-    position: 'absolute',
-    top: -Utils.sectionBorderHeight(),
-    zIndex: 2,
-    width: 0,
-    height: 0,
-    borderLeft: `${Utils.sectionBorderWidth()}px solid transparent`,
-    borderBottom: `${Utils.sectionBorderHeight()}px solid #2c3e50`,
-  }
-  return (<div style={style}></div>)
-}
-
-const BottomTriangle = () => {
-  const style = {
-    position: 'absolute',
-    // top: Utils.sectionBorderHeight(),
-    zIndex: 2,
-    width: 0,
-    height: 0,
-    borderRight: `${Utils.sectionBorderWidth()}px solid transparent`,
-    borderTop: `${Utils.sectionBorderHeight()}px solid #2c3e50`,
-  }
-  return (<div style={style}></div>)
-}
-
-class Statement extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  handleResize() {this.setState({}); }
-
-  render() {
-    return (
-      <div className="Statement">
-        <EventListener target="window" onResize={this.handleResize.bind(this)} />
-        <TopTriangle></TopTriangle>
+  return (
+    <div className="Title" style={title_style}>
+      <div className="dot-pattern">
         <div className="container">
-          <p className="title code">{'> make $you smile;'}</p>
-          <div className="statement">
-            <p>三度の飯くらいプログラミングが好きなサラリーマンが、</p>
-            <p>アイデアの力でどこまで飛べるのか。</p>
-            <p>最近仲間になった「ディープラーニング」を携えて、</p>
-            <p>今日も考え、作り、動かす。</p>
+          <div className="logo" style={logo_style}>
+            <img 
+            className="hide-on-small-only" 
+            src="/images/logo_yoko.svg"
+            alt="ディープサラリーマン村上晋太郎" />
+            <img 
+            className="hide-on-med-and-up" 
+            src="/images/logo_tate.svg"
+            alt="ディープサラリーマン村上晋太郎" />
           </div>
         </div>
-        <BottomTriangle></BottomTriangle>
+      <TopTriangle color='#2c3e50' />
       </div>
-      )
+    </div>
+  )
+};
+
+const TopTriangle = (props) => {
+  const {color} = props;
+  const width = 100;
+  const height = width * Math.tan(Math.PI/12);
+  const points = [
+    0, height,
+    width, height,
+    width, 0,
+  ]
+  const style = {
+    verticalAlign: 'bottom',
   }
+  return (
+    <svg viewBox={[0, 0, width, height].join(' ')} style={style}>
+      <polygon points={points.join(',')} stroke="none" fill={color} />
+    </svg>
+    )
 }
+
+const BottomTriangle = (props) => {
+  const {color} = props;
+  const width = 100;
+  const height = width * Math.tan(Math.PI/12);
+  const points = [
+    0, 0,
+    width, 0,
+    0, height,
+  ]
+  const style = {
+    verticalAlign: 'top',
+  }
+  return (
+    <svg viewBox={[0, 0, width, height].join(' ')} style={style}>
+      <polygon points={points.join(',')} stroke="none" fill={color} />
+    </svg>
+    )
+}
+
+const Statement = () =>  {
+  return (
+    <div className="Statement">
+      <div className="container">
+        <p className="title code">{'> make $you smile;'}</p>
+        <div className="statement">
+          <p>三度の飯くらいプログラミングが好きなサラリーマンが、</p>
+          <p>アイデアの力でどこまで飛べるのか。</p>
+          <p>最近仲間になった「ディープラーニング」を携えて、</p>
+          <p>今日も考え、作り、動かす。</p>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 class Works extends Component {
   handleResize() {this.setState({}); }
@@ -148,8 +124,8 @@ class Works extends Component {
     const Background = () => {
       var style = {
         position: 'fixed',
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: '100%',
+        height: '100%',
         top: 0,
         zIndex: -1,
         backgroundImage: 'url(/images/mft.jpg)',
@@ -195,15 +171,16 @@ class Works extends Component {
       )}
 
     return (
-      <div className="Works"style={{paddingTop: Utils.sectionBorderHeight(), paddingBottom: Utils.sectionBorderHeight()}}>
-        <EventListener target="window" onResize={this.handleResize.bind(this)} />
+      <div className="Works">
         <Background></Background>
+        <BottomTriangle color='#2c3e50'></BottomTriangle>
         <div className="workscontainer">
           <p className="title code">WORKS</p>
           <div className="row flex">
             {works.map(makeWorkCard)}
           </div>
         </div>
+        <TopTriangle color='#2c3e50' />
       </div>
       )
   }
@@ -220,59 +197,53 @@ class Work extends Component {
 }
 
 
-class History extends Component {
-  handleResize() {this.setState({}); }
+const History = () => {
+  const {studies, experiences} = require('./data/history');
 
-  render() {
-    const {studies, experiences} = require('./data/history');
+  const makeStudyCard = (data, index) => (
+    <div className="elem col s12 m6 l4" key={index}>
+      <div className="card blue-grey darken-4">
+        <div className="card-content">
+          <span className="card-title">{data.title}</span>
+          <p className='time'>{data.time}</p>
+          <p className='place' dangerouslySetInnerHTML={{__html: data.place}}></p>
+          <p className='text'>{data.text}</p>
+        </div>
+      </div>
+    </div>
+    )
 
-    const makeStudyCard = (data, index) => (
-      <div className="elem col s12 m6 l4" key={index}>
-        <div className="card blue-grey darken-4">
-          <div className="card-content">
-            <span className="card-title">{data.title}</span>
-            <p className='time'>{data.time}</p>
-            <p className='place' dangerouslySetInnerHTML={{__html: data.place}}></p>
-            <p className='text'>{data.text}</p>
+  const makeExperienceCard = (data, index) => (
+    <div className="elem col s12 m6 l4" key={index}>
+      <div className="card blue-grey darken-4">
+        <div className="card-content">
+          <span className="card-title">{data.title}</span>
+          <p className='time'>{data.time}</p>
+          <p className='text'>{data.job}</p>
+        </div>
+      </div>
+    </div>
+    )
+
+
+  return (
+    <div className="History">
+      <div className="container">
+        <p className="title code">STUDY</p>
+        <div className="study">
+          <div className="row flex">
+            {studies.map(makeStudyCard)}
+          </div>
+        </div>
+        <p className="title code">EXPERIENCE</p>
+        <div className="study">
+          <div className="row flex">
+            {experiences.map(makeExperienceCard)}
           </div>
         </div>
       </div>
-      )
-
-    const makeExperienceCard = (data, index) => (
-      <div className="elem col s12 m6 l4" key={index}>
-        <div className="card blue-grey darken-4">
-          <div className="card-content">
-            <span className="card-title">{data.title}</span>
-            <p className='time'>{data.time}</p>
-            <p className='text'>{data.job}</p>
-          </div>
-        </div>
-      </div>
-      )
-
-
-    return (
-      <div className="History">
-        <EventListener target="window" onResize={this.handleResize.bind(this)} />
-        <TopTriangle></TopTriangle>
-        <div className="container">
-          <p className="title code">STUDY</p>
-          <div className="study">
-            <div className="row flex">
-              {studies.map(makeStudyCard)}
-            </div>
-          </div>
-          <p className="title code">EXPERIENCE</p>
-          <div className="study">
-            <div className="row flex">
-              {experiences.map(makeExperienceCard)}
-            </div>
-          </div>
-        </div>
-      </div>
-      )
-  }
-}
+    </div>
+  )
+};
 
 export default App;
