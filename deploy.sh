adcb8fea8e1f36d06062616931e0fee8e4f772a1
s.murakami <smurakami.84@gmail.com>
2020-03-16 19:28:38 +0900

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 git push origin master
-ssh smurakami.com "cd /var/www/smurakami.com; git pull origin master; yarn build"
+ssh smurakami.com "cd /var/www/smurakami.com; git pull origin master; /home/smurakami/.ndenv/shims/yarn build"
